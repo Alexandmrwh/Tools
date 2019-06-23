be093a358693a186b6b4e76c89ade671b2943bbc
Alex <wuhancs@pku.edu.cn>
2019-06-23 21:39:06 +0800

    convert txt to csv

---
+++ txt2csv.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('test.txt', 'r') as in_file:
+    stripped = (line.strip() for line in in_file)
+    lines = (line.split(",") for line in stripped if line)
+    with open('test_ids.csv', 'w') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerows(lines)
